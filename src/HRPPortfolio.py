4eeaa380d7f6acba725c9918a869b36d62d355ee
frangi2002 <88388590+frangi2002@users.noreply.github.com>
2024-11-15 16:33:19 -0500

    distance matrix and eucledian matrix draft done

--- src/HRPPortfolio.py
+++ src/HRPPortfolio.py
@@ -2,7 +2,8 @@
 import numpy as np
 import pandas as pd
 import scipy.cluster.hierarchy as sch
-import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import euclidean_distances
+
 
 """
 This file contains the HRPPortfolio class. This class will have the following attributes:
@@ -15,13 +16,41 @@ class HRPPortfolio:
         self.corelation_matrix = corelation_matrix
     
     def distance_matrix(self, correlation_matrix):
+        """
+        Parameters
+        ----------
+        correlation_matrix : pd.DataFrame 
+            Datafrae containing the correlation matrix of the assets
+
+        Returns
+        -------
+        Correlation-Distance matrix
+        """
+        distance = (0.5*(1- correlation_matrix))**0.5
         return distance
     
     def eucledian_distance_matrix(self, correlation_matrix):
-        return eucledian_distance 
+        """
+        Parameters
+        ----------
+        correlation_matrix : pd.DataFrame 
+            Datafrae containing the correlation matrix of the assets
+
+        Returns
+        -------
+        Correlation-Distance matrix
+        """
+
+        # Calculate the distance matrix
+        distance = self.distance_matrix(correlation_matrix) # Possible redundancy, distance matrix is being calculated twice
+
+        # Calculate the eucledian distance matrix
+        euclidean_distance = euclidean_distances(distance.values)
+
+        return euclidean_distance 
 
     def hierarchical_clustering(self):
-        return linkage_matrix~
+        return linkage_matrix
 
     def quasi_diagonalization(self, linkage_matrix):
         return clustered_correlations
