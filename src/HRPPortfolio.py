ed302efc3b8901d7343eb6c1b51bcadfbff72693
frangi2002 <88388590+frangi2002@users.noreply.github.com>
2024-11-15 15:53:51 -0500

    adding the HRP structure file

---
+++ src/HRPPortfolio.py
@@ -0,0 +1,51 @@
+# Necessary Dependancies
+import numpy as np
+import pandas as pd
+import scipy.cluster.hierarchy as sch
+import matplotlib.pyplot as plt
+
+"""
+This file contains the HRPPortfolio class. This class will have the following attributes:
+1. 
+"""
+
+class HRPPortfolio:
+
+    def __init__(self, correlation_matrix):
+        self.corelation_matrix = corelation_matrix
+    
+    def distance_matrix(self, correlation_matrix):
+        return distance
+    
+    def eucledian_distance_matrix(self, correlation_matrix):
+        return eucledian_distance 
+
+    def hierarchical_clustering(self):
+        return linkage_matrix~
+
+    def quasi_diagonalization(self, linkage_matrix):
+        return clustered_correlations
+    
+    def recursive_bisection(self, clustered_correlations):
+        return weights
+
+
+
+"""
+Parameters
+----------
+input1 : dtype 
+    what is it?
+input2 : dtype 
+    what is it?
+input3 : dtype 
+    what is it?
+
+Returns
+-------
+What does the function return.
+"""
+    
+
+    
+        
