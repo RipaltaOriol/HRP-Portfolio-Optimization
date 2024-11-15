250d19afd913c7b0d246af7122c0842d0ba84a0f
frangi2002 <88388590+frangi2002@users.noreply.github.com>
2024-11-15 17:50:16 -0500

    changing the HRPPortfolio class

--- src/HRPPortfolio.py
+++ src/HRPPortfolio.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import scipy.cluster.hierarchy as sch
 from sklearn.metrics.pairwise import euclidean_distances
+from scipy.cluster.hierarchy import dendrogram, linkage
+from src import RelationalStatistics
 
 
 """
@@ -10,47 +12,25 @@ This file contains the HRPPortfolio class. This class will have the following at
 1. 
 """
 
-class HRPPortfolio:
+class HRPPortfolio():
 
-    def __init__(self, correlation_matrix, covariance_matrix):
-        self.covariance_matrix = covariance_matrix
-        self.corelation_matrix = correlation_matrix
-    
-    def distance(self, correlation_matrix):
-        """
-        Parameters
-        ----------
-        correlation_matrix : pd.DataFrame 
-            Datafrae containing the correlation matrix of the assets
-
-        Returns
-        -------
-        Correlation-Distance matrix
-        """
-        distance_matrix = (0.5*(1- correlation_matrix))**0.5
-        return distance_matrix
-    
-    def eucledian_distance(self, correlation_matrix):
-        """
-        Parameters
-        ----------
-        correlation_matrix : pd.DataFrame 
-            Datafrae containing the correlation matrix of the assets
-
-        Returns
-        -------
-        Correlation-Distance matrix
-        """
-
-        # Calculate the distance matrix
-        distance_matrix = self.distance(correlation_matrix) # Possible redundancy, distance matrix is being calculated twice
-
-        # Calculate the eucledian distance matrix
-        euclidean_distance_matrix = euclidean_distances(distance_matrix.values)
-
-        return euclidean_distance_matrix 
+    def __init__(self):
+        self.stats_module = RelationalStatistics()
 
     def hierarchical_clustering(self):
+        """
+        Parameters
+        ----------
+        self
+
+        Returns
+        -------
+        Correlation-Distance matrix
+        """
+
+        # Calculate the eucledian distance between the stocks - will be using centroid linkage method
+        linkage_matrix = linkage(self.stats_module.fetch_eucledian_distance(), 'centroid')
+
         return linkage_matrix
 
     def quasi_diagonalization(self, cluster_order):
