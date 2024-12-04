bc9290585955dcf4bf5aa0d1d7641c5d5ed9257d
Oriol Ripalta Maso <oriolripalta@hotmail.com>
2024-12-03 22:13:39 -0500

    add: ticker provider module

--- src/utils/TickerSelector.py
+++ src/utils/TickerSelector.py
@@ -2,6 +2,7 @@ import pandas as pd
 import ssl
 import random
 import yfinance as yf
+from utils.DataProvider import DataProvider
 
 class TickerSelector:
     def __init__(self):
@@ -28,46 +29,44 @@ class TickerSelector:
             print(f"Error fetching S&P 500 tickers: {e}")
             return []
         
-    def random_selection(self, num_stocks=100):
-        """random tickers"""
-        return random.sample(self.tickers, num_stocks)
+
     
-    def calculate_ticker_returns(self, start_date, end_date, num_stocks=100):
+    def calculate_ticker_returns(self, start_date, end_date):
         """ calc returns of sp500 in order to choose top/bottom"""
-        ticker_data = {}
+        ticker_data = []
         for ticker in self.tickers:
             ticker = ticker.replace('.', '-') # different convention on wiki VS yfinance
-            try:
-                stock = yf.Ticker(ticker)
-                # Fetch historical data for the custom date range
-                history = stock.history(start=start_date, end=end_date)
-                if not history.empty:
-                    # Calculate total return over the period
-                    returns = (history["Close"].iloc[-1] - history["Close"].iloc[0]) / history["Close"].iloc[0]
-                    ticker_data[ticker] = returns
-            except Exception as e:
-                print(f"Error fetching data for {ticker}: {e}")
-                print(ticker)
-        return ticker_data
+            ticker_data.append(ticker)
+            # try:
+            #     stock = yf.Ticker(ticker)
+            #     # Fetch historical data for the custom date range
+            #     history = stock.history(start=start_date, end=end_date)
+            #     if not history.empty:
+            #         # Calculate total return over the period
+            #         returns = (history["Close"].iloc[-1] - history["Close"].iloc[0]) / history["Close"].iloc[0]
+            #         ticker_data[ticker] = returns
+            # except Exception as e:
+            #     print(f"Error fetching data for {ticker}: {e}")
+            #     print(ticker)
+        provider = DataProvider(start_date, end_date, ticker_data)
+        data = provider.provide()
+        return data
+
+        # return ticker_data
     
+    def random_selection(self,ticker_data, num_stocks=100):
+        """random tickers"""
+        return random.sample(list(ticker_data.columns), num_stocks)
+    
+
     def select_top_return_tickers(self, ticker_data, num_stocks=100):
         """
         top tickers by returns 
         """
         # sort descending order
-        sorted_tickers = sorted(ticker_data.items(), key=lambda x: x[1], reverse=True)
-        return [ticker[0] for ticker in sorted_tickers[:num_stocks]]
-    
-    def select_bottom_tickers(self, ticker_returns, num_stocks=100):
-        """
-            Select the bottom tickers 
-        """
-        # Sort tickers by returns in ascending order
-        sorted_tickers = sorted(ticker_returns.items(), key=lambda x: x[1])
-
-        # Select the bottom tickers
-        return [ticker[0] for ticker in sorted_tickers[:num_stocks]]
-
-
+        rets = ticker_data.agg("sum")
+        rets.sort_values(ascending=False, inplace = True)
+        # sorted_tickers = sorted(ticker_data.items(), key=lambda x: x[1], reverse=True)
+        return rets.index[:num_stocks]
 
     
